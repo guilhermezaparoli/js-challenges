a4cc77c816c0615256b9d963af408000b2d97132
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-17 16:28:16 -0300

    Recursive fibonacci and factorial solutions

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,13 @@
-function fibonacci() {}
+function fibonacci(position) {
+    
+    if(position < 2){
+        return position
+    }
+
+    return fibonacci(position -1) + fibonacci(position - 2)
+}
 
 module.exports = fibonacci;
+
+
+console.log(fibonacci(9))
--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,3 +1,13 @@
-function factorial() {}
+// function factorial(num) {
+//     if (num < 2) {
+//         return 1
+//     }
+
+//     return num * factorial(num - 1)
+// }
+
+
+const factorial = (num) => num < 2 ? 1 : num * factorial(num -1)
+
 
 module.exports = factorial;
