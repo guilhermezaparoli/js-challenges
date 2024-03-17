7a924cc3cace301805bcdcd3665f3a3a4d2626fe
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-17 19:08:12 -0300

    Recursive array-sum solution

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,18 @@
-function arraySum() {}
+// function arraySum(arr) {
+//     console.log(arr);
+ 
+//     if(arr.length <= 0){
+//         return 0
+//     }
+
+
+   
+
+//     return  arraySum(arr.slice(1)) + arr[0]
+// }
+
+const arraySum = (arr) => arr.length <= 0 ? 0 : arraySum(arr.slice(1)) + arr[0]
+
 
 module.exports = arraySum;
+
