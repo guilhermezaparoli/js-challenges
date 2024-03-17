49ecaf488e03cda2ad19083a164ec144a1d7ff6a
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-17 17:59:18 -0300

    recursive power challenge solution

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,12 @@
-function power() {}
+// function power(base, exponent) {
+//     if (exponent == 1) {
+//         return base
+//     }
+
+//     return power(base, exponent - 1) * base
+// }
+
+
+const power = (base, exponent) => exponent == 1 ? base : power(base, exponent - 1) * base
 
 module.exports = power;
