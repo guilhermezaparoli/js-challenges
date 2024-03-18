01bc54a58713d27571fd3595e2a08be508e6b9ba
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-18 14:42:19 -0300

    flatten-array solution

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,20 @@
-function flattenArray() {}
+function flattenArray(arr) {
+
+    let result = []
+
+
+    for (const item of arr) {
+        if (Array.isArray(item)) {
+            result = result.concat(flattenArray(item))
+        } else {
+            result.push(item)
+        }
+    }
+
+    return result
+}
 
 module.exports = flattenArray;
+
+
+console.log(flattenArray([1, [2, 3], [4, 5, [6]]]));
