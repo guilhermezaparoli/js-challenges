2d42d179f4c35e0c55c5d92471b6d1fc9fed5c78
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-18 13:56:10 -0300

    numberRange solution

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,15 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+    if (startNum == endNum) {
+        return [endNum]
+    }
+    const numbers = numberRange(startNum + 1, endNum)
+    numbers.unshift(startNum)
+
+    return numbers
+}
 
 module.exports = numberRange;
+
+
+console.log(numberRange(1, 5))
+
