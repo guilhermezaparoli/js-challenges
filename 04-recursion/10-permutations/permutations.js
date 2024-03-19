f5a454ea41b438fdf332cf7e6d24822807d687ab
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-19 15:22:29 -0300

    permutations solution

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -2,13 +2,25 @@ function permutations(str) {
     
 let result = []
 
-    for(const [letter] in str){
-        console.log(str[letter])
-    }
+if(str.length === 0) {
+    result.push("")
+    return result;
 }
+
+    for(let i = 0; i < str.length; i++){
+        const firstChar = str[i]
+        const restOfString = str.slice(0, i) + str.slice(i + 1)
+        const subPermutations = permutations(restOfString)
+
+        for(let j = 0; j < subPermutations.length; j++) {
+            result.push(firstChar + subPermutations[j])
+        }
+    }
+    return result
+}
+
+console.log(permutations("abc"))
 
 module.exports = permutations;
 
 
-
-console.log(permutations("abc"))
