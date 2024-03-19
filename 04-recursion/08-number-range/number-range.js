e8072160242a7d5005634ca3d9174ed32596a00e
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-03-19 13:44:34 -0300

    number-range solution

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -2,6 +2,7 @@ function numberRange(startNum, endNum) {
     if (startNum == endNum) {
         return [endNum]
     }
+    
     const numbers = numberRange(startNum + 1, endNum)
     numbers.unshift(startNum)
 
--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,14 @@
-function permutations() {}
+function permutations(str) {
+    
+let result = []
+
+    for(const [letter] in str){
+        console.log(str[letter])
+    }
+}
 
 module.exports = permutations;
+
+
+
+console.log(permutations("abc"))
